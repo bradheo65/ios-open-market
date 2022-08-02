292590f33f87e3b2e93b7fe433c7918373b90c23
BaekGom <jihunmm02@gmail.com>
2022-08-02 14:47:39 +0900

    test: 테스트 브렌치

--- OpenMarket/OpenMarket/ViewController/AddProductViewController.swift
+++ OpenMarket/OpenMarket/ViewController/AddProductViewController.swift
@@ -11,69 +11,80 @@ class AddProductViewController: UIViewController {
     
     @IBOutlet weak var addImageCollectionView: UICollectionView!
 
+    @IBOutlet weak var addImageButton: UIButton!
+    @IBOutlet weak var horizonStackView: UIStackView!
+    
     let imagePicker = UIImagePickerController()
     var numberOfCell = 10
     
+    private var horizontalUIView: UIView {
+        get {
+            let uiView = UIView()
+            uiView.translatesAutoresizingMaskIntoConstraints = false
+            return uiView
+        }
+        set{}
+    }
+    
+    private var emptyStackView: UIStackView {
+        get {
+            let stackview = UIStackView()
+            stackview.translatesAutoresizingMaskIntoConstraints = false
+            return stackview
+        }
+        set {}
+    }
+    
+    private var postImageView: UIImageView {
+        get {
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFill
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }
+        set{}
+    }
+    
+    func addImageView() {
+        self.horizontalUIView.addSubview(postImageView)
+        self.horizonStackView.addSubview(horizontalUIView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.imagePicker.sourceType = .photoLibrary
-//        self.imagePicker.allowsEditing = true
-//        self.imagePicker.delegate = self
-//        self.addImageCollectionView.delegate = self
-//        self.addImageCollectionView.dataSource = self
+        self.imagePicker.allowsEditing = true
+        self.imagePicker.delegate = self
+
     }
     
     @IBAction func back(_ sender: UIButton) {
         self.presentingViewController?.dismiss(animated: true)
     }
     
-    @objc func pickImage() {
-        self.present(self.imagePicker, animated: true)
+    func pickImage() {
+        self.imagePicker.sourceType  = .photoLibrary
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func loadImageFromLibrary(_ sender: UIButton) {
+        pickImage()
     }
 }
 
-//extension AddProductViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//
-//        return numberOfCell
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AddProductCollectionViewCell.reuseIdentifier, for: indexPath) as! AddProductCollectionViewCell
-//        cell.backgroundColor = .black
-//        //cell.profileImageView.image = .add
-//        return cell
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        numberOfCell += 1
-//        collectionView.reloadData()
-//    }
-//}
-//
-//extension AddProductViewController: UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let flowayout = collectionViewLayout as? UICollectionViewFlowLayout
-//        flowayout?.scrollDirection = .horizontal
-//        flowayout?.minimumLineSpacing = 20
-//        flowayout?.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-//        return CGSize(width: view.frame.width - 100, height: view.frame.height - 150)
-//    }
-//}
-
-//extension AddProductViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-//
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//
-//        var newImage: UIImage? = nil
-//
-//        if let possibleImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-//            newImage = possibleImage
-//        } else if let possibleImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-//            newImage = possibleImage
-//        }
-//
-//        //profileImageView.image = newImage
-//    }
-//}
+extension AddProductViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        var newImage: UIImage? = nil
+        
+        if let possibleImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            newImage = possibleImage // 원본 이미지가 있을 경우
+        }
+        
+        self.postImageView.image = newImage // 받아온 이미지를
+        addImageView()
+        picker.dismiss(animated: true, completion: nil) // picker를 닫아줌
+    }
+}
